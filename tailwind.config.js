0c58d6166ae6404a97d1ad98219e3f59653892a4
webmaruf2022 <webmaruf2022@gmail.com>
2022-07-04 16:25:42 +0600

    Design in Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        'green-i':'#91C610',
+        'blue-i':'#016FD3',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
